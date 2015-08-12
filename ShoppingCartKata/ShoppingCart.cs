5ebcfe420d26dab34fb98967e02b1a8431b3b483
PaulBoocock <paulboocock@codeweavers.net>
2015-08-12 21:27:23 +0100

    Initial commit with base Shopping Cart and first failing test.

---
+++ ShoppingCartKata/ShoppingCart.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace ShoppingCartKata
+{
+    [TestFixture]
+    public class ShoppingCartTests
+    {
+        [Test]
+        public void WhenOneAppleThenTotalIs50()
+        {
+            var subject = new ShoppingCart();
+            subject.Scan("Apple");
+            Assert.That(subject.Total(), Is.EqualTo(50));
+        }
+    }
+
+    public class ShoppingCart
+    {
+        public int Total()
+        {
+            return 0;
+        }
+
+        public void Scan(string code)
+        {
+        }
+    }
+}
